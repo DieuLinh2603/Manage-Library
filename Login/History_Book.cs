b80b08fa9ec3fbb8d2382914067eb990052426d0
NTPhus <phu444080@gmail.com>
2024-11-16 11:50:35 +0700

    Update features

--- Login/History_Book.cs
+++ Login/History_Book.cs
@@ -12,9 +12,46 @@ namespace Login
 {
     public partial class History_Book : Form
     {
+        LVNDataContext db = new LVNDataContext();
+        int MaxNgayMuon = 90;
         public History_Book()
         {
             InitializeComponent();
+        }
+
+        private void History_Book_Load(object sender, EventArgs e)
+        {
+            var rsm = from a in db.LichSuMuonTraSaches
+                      from b in db.Saches
+                      from c in db.DocGias
+                      where a.NgayTra == null && a.SoThe == c.SoThe && a.MaSach == b.MaSach
+                      select new{ 
+                        a.id,
+                        a.SoThe,
+                        c.HoTen,
+                        c.Email,
+                        b.TenSach,
+                        a.NgayMuon,
+                        a.NgayTra
+                        };
+            dtgv_MuonSach.DataSource = rsm;
+
+            var rst = from a in db.LichSuMuonTraSaches
+                      from b in db.Saches
+                      from c in db.DocGias
+                      where a.NgayTra != null && a.SoThe == c.SoThe && a.MaSach == b.MaSach
+                      select new
+                      {
+                          a.id,
+                          a.SoThe,
+                          c.HoTen,
+                          c.Email,
+                          b.TenSach,
+                          a.NgayMuon,
+                          a.NgayTra,
+                          PhiPhat = (a.NgayTra - a.NgayMuon).Value.Days > MaxNgayMuon ? b.GiaSach : 0
+                      };
+            dtgv_TraSach.DataSource = rst;
         }
     }
 }
