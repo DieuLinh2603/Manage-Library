4c0f975041b856cdb9270f708f31a21d5a2600d6
NTPhus <phu444080@gmail.com>
2024-11-20 21:28:19 +0700

    update button thoat

--- Login/Home.cs
+++ Login/Home.cs
@@ -62,7 +62,7 @@ namespace Login
             DialogResult f = MessageBox.Show("Bạn có thực sự muốn thoát không", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
             if(f == DialogResult.Yes)
             {
-                this.Hide();
+                Application.Exit();
             }
             else
             {
